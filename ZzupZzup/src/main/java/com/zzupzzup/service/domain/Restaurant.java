6db3c950a9eeedb75f81caee6aab8ba3dbf062fb
김관우 <@DESKTOP-T4KGO60>
2021-12-16 19:59:03 +0900

    kwanwoo : domain 중 Restaurant와 community 작성
    2021-12-16

--- ZzupZzup/src/main/java/com/zzupzzup/service/domain/Restaurant.java
+++ ZzupZzup/src/main/java/com/zzupzzup/service/domain/Restaurant.java
@@ -1,9 +1,228 @@
 package com.zzupzzup.service.domain;
 
-public class Restaurant {
+import java.util.Date;
+import java.util.List;
 
+public class Restaurant {
+	
+	///Field
+	private int restaurantNo;
+	private String restaurantName;
+	private String restaurantTel;
+	//////////// EL 적용 ////////////
+	private String restaurantTel1;
+	private String restaurantTel2;
+	private String restaurantTel3;
+	////////////////////////////////
+	private List<String> restaurantImage;
+	private int menuType;
+	private boolean mainMenuStatus;
+	private String ownerName;
+	private String ownerImage;
+	private String restaurantText;
+	private boolean reservationStatus;
+	private boolean parkable;
+	private Date requestDate;
+	private Date responseDate;
+	private String streetAddress;
+	private String areaAddress;
+	private String restAddress;
+	
+	
+	//Constructor
 	public Restaurant() {
 		// TODO Auto-generated constructor stub
 	}
 
+
+	public int getRestaurantNo() {
+		return restaurantNo;
+	}
+
+
+	public void setRestaurantNo(int restaurantNo) {
+		this.restaurantNo = restaurantNo;
+	}
+
+
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+
+
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+
+
+	public String getRestaurantTel() {
+		return restaurantTel;
+	}
+
+
+	public void setRestaurantTel(String restaurantTel) {
+		this.restaurantTel = restaurantTel;
+	}
+
+
+	public String getRestaurantTel1() {
+		return restaurantTel1;
+	}
+
+
+	public void setRestaurantTel1(String restaurantTel1) {
+		this.restaurantTel1 = restaurantTel1;
+	}
+
+
+	public String getRestaurantTel2() {
+		return restaurantTel2;
+	}
+
+
+	public void setRestaurantTel2(String restaurantTel2) {
+		this.restaurantTel2 = restaurantTel2;
+	}
+
+
+	public String getRestaurantTel3() {
+		return restaurantTel3;
+	}
+
+
+	public void setRestaurantTel3(String restaurantTel3) {
+		this.restaurantTel3 = restaurantTel3;
+	}
+
+
+	public List<String> getRestaurantImage() {
+		return restaurantImage;
+	}
+
+
+	public void setRestaurantImage(List<String> restaurantImage) {
+		this.restaurantImage = restaurantImage;
+	}
+
+
+	public int getMenuType() {
+		return menuType;
+	}
+
+
+	public void setMenuType(int menuType) {
+		this.menuType = menuType;
+	}
+
+
+	public boolean isMainMenuStatus() {
+		return mainMenuStatus;
+	}
+
+
+	public void setMainMenuStatus(boolean mainMenuStatus) {
+		this.mainMenuStatus = mainMenuStatus;
+	}
+
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+
+	public String getOwnerImage() {
+		return ownerImage;
+	}
+
+
+	public void setOwnerImage(String ownerImage) {
+		this.ownerImage = ownerImage;
+	}
+
+
+	public String getRestaurantText() {
+		return restaurantText;
+	}
+
+
+	public void setRestaurantText(String restaurantText) {
+		this.restaurantText = restaurantText;
+	}
+
+
+	public boolean isReservationStatus() {
+		return reservationStatus;
+	}
+
+
+	public void setReservationStatus(boolean reservationStatus) {
+		this.reservationStatus = reservationStatus;
+	}
+
+
+	public boolean isParkable() {
+		return parkable;
+	}
+
+
+	public void setParkable(boolean parkable) {
+		this.parkable = parkable;
+	}
+
+
+	public Date getRequestDate() {
+		return requestDate;
+	}
+
+
+	public void setRequestDate(Date requestDate) {
+		this.requestDate = requestDate;
+	}
+
+
+	public Date getResponseDate() {
+		return responseDate;
+	}
+
+
+	public void setResponseDate(Date responseDate) {
+		this.responseDate = responseDate;
+	}
+
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+
+	public String getAreaAddress() {
+		return areaAddress;
+	}
+
+
+	public void setAreaAddress(String areaAddress) {
+		this.areaAddress = areaAddress;
+	}
+
+
+	public String getRestAddress() {
+		return restAddress;
+	}
+
+
+	public void setRestAddress(String restAddress) {
+		this.restAddress = restAddress;
+	}
+	
+	
 }
---
+++ ZzupZzup/src/main/java/com/zzupzzup/service/domain/RestaurantTime.java
@@ -0,0 +1,9 @@
+package com.zzupzzup.service.domain;
+
+public class RestaurantTime {
+
+	public RestaurantTime() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
