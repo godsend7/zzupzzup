e3ffed51499e7af5c51d231d5494217b67a9d3dc
김관우 <@DESKTOP-T4KGO60>
2021-12-17 09:56:03 +0900

    kwanwoo : new branch Create (develop)
    2021-12-17 09:55am

--- ZzupZzup/src/main/java/com/zzupzzup/service/restaurant/RestaurantService.java
+++ ZzupZzup/src/main/java/com/zzupzzup/service/restaurant/RestaurantService.java
@@ -1,5 +1,29 @@
 package com.zzupzzup.service.restaurant;
 
-public interface RestaurantService {
+import com.zzupzzup.service.domain.Restaurant;
+import com.zzupzzup.service.domain.RestaurantMenu;
+import com.zzupzzup.service.domain.RestaurantTime;
 
+public interface RestaurantService {
+	
+	public void addRestaurant(Restaurant restaurant) throws Exception;
+	
+	public Restaurant getRestaurant(int restaurantNo) throws Exception;
+	
+	public void listRestaurant() throws Exception;
+	
+	public void updateRestaurant(Restaurant restaurant) throws Exception;
+	
+	public void deleteRestaurant(Restaurant restaurant) throws Exception;
+	
+	public void listCallDibs() throws Exception;
+	
+	public void checkCallDibs() throws Exception;
+	
+	public void cancelCallDibs() throws Exception;
+	
+	public RestaurantMenu getRestaurantMenu(RestaurantMenu restaurantMenu) throws Exception;
+	
+	public RestaurantTime getRestaurantTime(RestaurantTime restaurantTime) throws Exception;
+	
 }
--- ZzupZzup/src/main/java/com/zzupzzup/service/restaurant/impl/RestaurantDAOImpl.java
+++ ZzupZzup/src/main/java/com/zzupzzup/service/restaurant/impl/RestaurantDAOImpl.java
@@ -1,11 +1,74 @@
 package com.zzupzzup.service.restaurant.impl;
 
+import com.zzupzzup.service.domain.Restaurant;
+import com.zzupzzup.service.domain.RestaurantMenu;
+import com.zzupzzup.service.domain.RestaurantTime;
 import com.zzupzzup.service.restaurant.RestaurantDAO;
 
 public class RestaurantDAOImpl implements RestaurantDAO {
 
 	public RestaurantDAOImpl() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addRestaurant(Restaurant restaurant) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Restaurant getRestaurant(int restaurantNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void listRestaurant() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRestaurant(Restaurant restaurant) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteRestaurant(Restaurant restaurant) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listCallDibs() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkCallDibs() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelCallDibs() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getRestaurantMenu(RestaurantMenu restaurantMenu) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getRestaurantTime(RestaurantTime restaurantTime) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
