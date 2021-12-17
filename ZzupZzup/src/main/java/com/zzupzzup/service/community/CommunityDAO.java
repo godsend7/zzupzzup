a10ae816f00781533621d92b13b0442b0f1030eb
김관우 <@DESKTOP-T4KGO60>
2021-12-17 17:50:08 +0900

    kwanwoo : Restaurant, Community DAO 및 Service 생성 and RestaurantMapper, CommunityMapper 생성
    2021-12-17 17:50

--- ZzupZzup/src/main/java/com/zzupzzup/service/community/CommunityDAO.java
+++ ZzupZzup/src/main/java/com/zzupzzup/service/community/CommunityDAO.java
@@ -1,5 +1,34 @@
 package com.zzupzzup.service.community;
 
-public interface CommunityDAO {
+import java.util.List;
 
+import com.zzupzzup.common.Search;
+import com.zzupzzup.service.domain.Community;
+
+public interface CommunityDAO {
+	
+	public int addCommunity(Community community) throws Exception;
+	
+	public Community getCommunity(int postNo) throws Exception;
+	
+	public List<Community> listCommunity(Search search) throws Exception;
+	
+	public int updateCommunity(Community community) throws Exception;
+	
+	public int deleteCommunity(Community community) throws Exception;
+	
+	public void addLike() throws Exception;
+	
+	public void deleteLike() throws Exception;
+	
+	public List<Community> listLike(Search search) throws Exception;
+	
+	public List<Community> listMyPost(Search search) throws Exception;
+	
+	public void officialCommunity() throws Exception;
+	
+	public void getLikeCount() throws Exception;
+	
+	public int getTotalCount(Search search) throws Exception;
+	
 }
