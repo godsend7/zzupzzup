1022593af665a08d8330748930be4ce585ba55af
Doyoun <Doyoun@DESKTOP-T9N0RLS>
2021-12-17 09:45:57 +0900

    doyoun: ChatDAO.java / ChatService.java / ChatDAOImpl.java / ChatService.java 생성중
    SQL initialize.sql 생성

--- ZzupZzup/src/main/java/com/zzupzzup/service/chat/impl/ChatDAOImpl.java
+++ ZzupZzup/src/main/java/com/zzupzzup/service/chat/impl/ChatDAOImpl.java
@@ -1,11 +1,93 @@
 package com.zzupzzup.service.chat.impl;
 
+import java.util.List;
+
+import com.zzupzzup.common.Search;
 import com.zzupzzup.service.chat.ChatDAO;
+import com.zzupzzup.service.domain.Chat;
 
 public class ChatDAOImpl implements ChatDAO {
 
 	public ChatDAOImpl() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addChat(Chat chat) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Chat getChat(int chatNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Chat> listChat(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateChat(Chat chat) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTotalCount(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Chat getChatEntrance(int chatNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteChat(Chat chat) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateChatState(Chat chat) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Chat getChatRecord(int chatNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateReadyCheck(Chat chat) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteChatMember(Chat chat) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Chat> listChatMember(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Chat> listReadyCheckMember(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
