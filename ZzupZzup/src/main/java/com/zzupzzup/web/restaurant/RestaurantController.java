d7b2348beb5b16e72f9911848a5707e773cf9c6a
godsend7 <highive00@gmail.com>
2021-12-24 19:07:03 +0900

    kwanwoo : Restaurant & Community Controller Update
    2021-12-24 19:06

--- ZzupZzup/src/main/java/com/zzupzzup/web/restaurant/RestaurantController.java
+++ ZzupZzup/src/main/java/com/zzupzzup/web/restaurant/RestaurantController.java
@@ -1,14 +1,63 @@
 package com.zzupzzup.web.restaurant;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.zzupzzup.service.domain.Restaurant;
+import com.zzupzzup.service.restaurant.RestaurantService;
+
+@Controller
+@RequestMapping("/restaurant/*")
 public class RestaurantController {
-
+	
+	///Field
+	@Autowired
+	@Qualifier("restaurantServiceImpl")
+	private RestaurantService restaurantService;
+	
+	
+	///Constructor
 	public RestaurantController() {
-		// TODO Auto-generated constructor stub
+		System.out.println(this.getClass());
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	///Method
+	@RequestMapping(value="addRestaurant", method=RequestMethod.GET)
+	public String addRestaurant() throws Exception {
+		
+		System.out.println("restaurant/addRestaurant : GET");
+		
+		return "forward:/restaurant/addRestaurantView.jsp";
 	}
-
+	
+	@RequestMapping(value="addRestaurant", method=RequestMethod.POST)
+	public String addRestaurant(@ModelAttribute("restaurant") Restaurant restaurant) throws Exception {
+		
+		System.out.println("restaurant/addRestaurant : POST");
+		
+		restaurantService.addRestaurant(restaurant);
+		
+		return "redirect:/restaurant/addRestaurant.jsp";
+	}
+	
+	@RequestMapping(value="getRestaurant", method=RequestMethod.GET)
+	public String getRestaurant(@RequestParam() int restaurantNo, Model model) throws Exception {
+		
+		System.out.println("restaurant/getRestaurant : GET");
+		
+		Restaurant restaurant = restaurantService.getRestaurant(restaurantNo);
+		
+		model.addAttribute("restaurant", restaurant);
+		
+		return "forward:/restaurant/getRestaurant.jsp";
+	}
+	
+	
 }
