7b1429f71dd66fbf211ef6c8dc7e81b5f40a4ab0
Doyoun <doyoun8813@gmail.com>
2021-12-25 10:03:39 +0900

    doyoun : chat 비지니스 로직 추가 rating 비지니스 로직 추가 restaurant SQL 변경
    2021-12-25 10:03

--- ZzupZzup/src/main/java/com/zzupzzup/service/rating/impl/RatingDAOImpl.java
+++ ZzupZzup/src/main/java/com/zzupzzup/service/rating/impl/RatingDAOImpl.java
@@ -1,11 +1,54 @@
 package com.zzupzzup.service.rating.impl;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.zzupzzup.common.Search;
+import com.zzupzzup.service.domain.Rating;
 import com.zzupzzup.service.rating.RatingDAO;
 
+@Repository("ratingDaoImpl")
 public class RatingDAOImpl implements RatingDAO {
+	
+	///Field
+	@Autowired
+	@Qualifier("sqlSessionTemplate")
+	private SqlSession sqlSession;
 
 	public RatingDAOImpl() {
-		// TODO Auto-generated constructor stub
+		System.out.println(this.getClass());
+	}
+	
+	//Method
+	@Override
+	public int addRating(Rating rating) throws Exception {
+		int result = sqlSession.insert("RatingMapper.addRating", rating);
+		
+		if(result == 1) {
+			return 1;
+		}else {
+			return 0;
+		}
+		
+	}
+	
+	@Override
+	public List<Rating> listRating(Map<String, Object> map) throws Exception {
+		System.out.println("ratingDaoImpl listRating map : " + map);
+		
+		List<Rating> list = sqlSession.selectList("RatingMapper.listRating", map);
+		
+		return list;
+	}
+	
+	@Override
+	public int getTotalCount(Search search) throws Exception {
+		return sqlSession.selectOne("RatingMapper.getTotalCount", search);
 	}
 
 }
