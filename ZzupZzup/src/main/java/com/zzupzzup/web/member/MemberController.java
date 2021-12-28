4ffd22f24ef1212ba0ba826c411471e7e33a9c42
czereio <y409813@gmail.com>
2021-12-28 20:16:07 +0900

    youngju : MemberController 수정
    login(), logout() 완료

--- ZzupZzup/src/main/java/com/zzupzzup/web/member/MemberController.java
+++ ZzupZzup/src/main/java/com/zzupzzup/web/member/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,27 +37,59 @@ public class MemberController {
 //		return "redirect:/member/loginView.jsp";
 //	}
 	
-	@RequestMapping( value="login", method=RequestMethod.POST )
-	public String login(@ModelAttribute("member") Member member , HttpSession session) throws Exception{
+//	@RequestMapping( value="login", method=RequestMethod.POST )
+//	public String login(@ModelAttribute("member") Member member , HttpSession session) throws Exception{
+//		
+//		System.out.println("/member/login : POST");
+//		//Business Logic
+//		Member mb = new Member();
+//		mb.setMemberId(member.getMemberId());
+//		memberService.getMember(mb);
+//		
+//		if( member.getPassword().equals(mb.getPassword())){
+//			session.setAttribute("mb", mb);
+//		}
+//		
+//		return "redirect:/main.jsp";
+//	}
+	
+	@RequestMapping( value="logout", method=RequestMethod.GET )
+	public String logout(HttpSession session) {
 		
-		System.out.println("/member/login : POST");
-		//Business Logic
-		Member mb = new Member();
-		mb.setMemberId(member.getMemberId());
-		memberService.getMember(mb);
+		System.out.println("/member/logout : GET");
 		
-		if( member.getPassword().equals(mb.getPassword())){
-			session.setAttribute("mb", mb);
+		session.invalidate();
+		
+		return "redirect:/main.jsp";
+		
+	}
+	
+	@RequestMapping(value="addMember/{memberRole}", method=RequestMethod.GET)
+	public String addMember(@PathVariable String memberRole) throws Exception {
+		
+		System.out.println("/member/addMember/"+memberRole+" : GET");
+		
+		return "redirect:/member/addMemberView.jsp?memberRole="+memberRole;
+		
+	}
+	
+	@RequestMapping(value="addMember", method=RequestMethod.POST)
+	public String addMember(@ModelAttribute("member") Member member) throws Exception {
+		
+		System.out.println("/member/addMember : POST");
+		
+		memberService.addMember(member);
+		System.out.println(member.getMemberRole());
+		if(member.getPushNickname() != null) {
+			//활동점수 추가하기
+			
+		}
+		if(member.getMemberRole() == "owner") {
+			//member domain과 같이 음식점 등록으로 페이지 넘기기
+			
 		}
 		
 		return "redirect:/main.jsp";
-	}
-	
-	public void logout() {
-		
-	}
-	
-	public void addMember() {
 		
 	}
 	
