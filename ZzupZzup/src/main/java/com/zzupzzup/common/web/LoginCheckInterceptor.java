72d694d73ce71f54b5f0d79607e6ae141479e2a6
HeeJooYoo <heejooyoo93@gmail.com>
2022-01-07 12:10:49 +0900

    heejoo : loginCheck 추가
    2022.01.07 12:10

--- ZzupZzup/src/main/java/com/zzupzzup/common/web/LoginCheckInterceptor.java
+++ ZzupZzup/src/main/java/com/zzupzzup/common/web/LoginCheckInterceptor.java
@@ -1,5 +1,7 @@
 package com.zzupzzup.common.web;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -21,30 +23,42 @@ public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
 		HttpSession session = request.getSession(true);
 		Member member = (Member)session.getAttribute("Member");
 		
-		if(member != null)  {
+		System.out.println("Logon Check :: " + member);
+		
+		if(member == null)  {
+			
 			String uri = request.getRequestURI();
 			
-			if(	uri.indexOf("addUser") != -1 ||	uri.indexOf("login") != -1 		|| 
-					uri.indexOf("checkDuplication") != -1 ){
-				request.getRequestDispatcher("/index.jsp").forward(request, response);
-				System.out.println("[ LoginCheckInterceptor end........]\n");
-				return false;
-			}
-			
-			System.out.println("[ LoginCheckInterceptor end........]\n");
-			return true;
-		} else {
-			String uri = request.getRequestURI();
-			
-			if(	uri.indexOf("addUser") != -1 ||	uri.indexOf("login") != -1 || uri.indexOf("checkDuplication") != -1 ){
-				System.out.println("[ LoginCheckInterceptor end........]\n");
+			if (uri.indexOf("favicon") != -1 || uri.indexOf("webjars") != -1 || uri.indexOf("resources") != -1 || uri.indexOf("member") != -1 || uri.indexOf("map") != -1 
+					|| uri.indexOf("getRestaurant") != -1 || uri.indexOf("listReview") != -1 || uri.indexOf("listReview") != -1) {
+				System.out.println("비회원 접근 가능 uri :: " + uri);
 				return true;
 			}
 			
-			request.getRequestDispatcher("/").forward(request, response);
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>alert('로그인이 필요합니다!'); location.href='/'; </script>");
+			out.flush();
+			out.close();
+			System.out.println("LoginCheck :: " + uri);
 			System.out.println("[ LoginCheckInterceptor end........]\n");
+			
+//			if(	uri.indexOf("listChat") != -1 || uri.indexOf("listCommunity") != -1 || uri.indexOf("listNotice") != -1 ){
+//				response.setContentType("text/html; charset=utf-8");
+//				PrintWriter out = response.getWriter();
+//				out.print("<script>alert('로그인이 필요합니다!'); location.href='/'; </script>");
+//				out.flush();
+//				out.close();
+//				System.out.println("LoginCheck :: " + uri);
+//				System.out.println("[ LoginCheckInterceptor end........]\n");
+//				return false;
+//			}
+//			
+//			System.out.println("[ LoginCheckInterceptor end........]\n");
 			return false;
 		}
+		
+		return true;
 	}
 
 }
